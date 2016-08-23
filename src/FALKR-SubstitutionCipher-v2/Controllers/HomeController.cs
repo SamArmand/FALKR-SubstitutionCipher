b65fda231735addf724fc2e5d65d690375703d90
Sam Assaf <sam.assaf@outlook.com>
2016-08-23 02:52:42 -0400

    Add project files.

---
+++ src/FALKR-SubstitutionCipher-v2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FALKR_SubstitutionCipher_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Encrypt()
+        {
+            return View();
+        }
+
+        public IActionResult Decrypt()
+        {
+            return View();
+        }
+    }
+}
