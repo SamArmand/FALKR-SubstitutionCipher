c37b648b0ccf0d21ca7af72e41be700fd8eb7c03
Sam Assaf <sam.assaf@icloud.com>
2016-08-24 22:55:00 -0400

    Made "About" its own view

---
+++ src/FALKR-SubstitutionCipher-v2/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FALKR_SubstitutionCipher_v2.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
