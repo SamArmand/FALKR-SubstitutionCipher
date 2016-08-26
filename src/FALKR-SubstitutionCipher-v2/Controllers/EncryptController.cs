0ae74b25451daa4b8e9621c322cac732fc27b2bd
Sam Assaf <sam.assaf@icloud.com>
2016-08-26 00:30:16 -0400

    encrypt model + controller

--- src/FALKR-SubstitutionCipher-v2/Controllers/EncryptController.cs
+++ src/FALKR-SubstitutionCipher-v2/Controllers/EncryptController.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FALKR_SubstitutionCipher_v2.Controllers
@@ -5,14 +7,58 @@ namespace FALKR_SubstitutionCipher_v2.Controllers
     public class EncryptController : Controller
     {
 
+        readonly char[] _alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
+
+        private char Replace(char v, string key)
+        {
+            for (var i = 0; i < _alphabet.Length; i++)
+                if (v == _alphabet.ElementAt(i))
+                    return key.ElementAt(i);
+
+            return v;
+ 
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Encrypt() 
+        [HttpPost]
+        public IActionResult Encrypt(EncryptModel model)
         {
-            return View("Index");
+            if (ModelState.IsValid)
+            {
+
+                model.Key = model.Key.ToUpper();
+                var key = model.Key;
+
+                var alphabet = new[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
+
+                var count = 27;
+
+                for (var i=0; i<alphabet.GetLength(0); i++)
+                {
+                    if (!key.Where((t, j) => key.ElementAt(j) == alphabet[i]).Any()) continue;
+                    count--;
+                    alphabet[i] = '.';
+                }
+
+                var plaintext = model.Plaintext.ToUpper();
+
+                var sb = new StringBuilder();
+
+                for (var i = 0; i < plaintext.Length; i++)
+                    sb.Append(Replace(plaintext.ElementAt(i), key));
+
+                model.Ciphertext = sb.ToString();
+
+                //TODO: push to view
+
+            }
+
+            // If we got this far, something failed; redisplay form.
+            return View(model);
         }
 
     }
---
+++ src/FALKR-SubstitutionCipher-v2/Models/EncryptModel.cs
@@ -0,0 +1,9 @@
+ public class EncryptModel
+ {
+    public string Plaintext { get; set; }
+
+    public string Key { get; set; }
+
+    public string Ciphertext { get; set; }
+
+ }
