e718c3ec3a3d49c7721a033b8b26011998d80764
Sam Assaf <sam.assaf@icloud.com>
2016-10-26 18:45:08 -0400

    fixed models

--- src/FALKR-SubstitutionCipher/Models/DecryptModel.cs
+++ src/FALKR-SubstitutionCipher/Models/DecryptModel.cs
@@ -1,13 +1,18 @@
- public class DecryptModel
- {
-    public string Plaintext { get; set; }
+using System.ComponentModel.DataAnnotations;
 
+public class DecryptModel
+ {
+    [Required]
+    public string Plaintext { get; set; }
+    
     public string Key { get; set; }
 
     public string Ciphertext { get; set; }
-
+    
+    [Required]
     public string From { get; set; }
 
+    [Required]
     public string To { get; set; }
 
  }
--- src/FALKR-SubstitutionCipher/Models/EncryptModel.cs
+++ src/FALKR-SubstitutionCipher/Models/EncryptModel.cs
@@ -1,7 +1,11 @@
- public class EncryptModel
+using System.ComponentModel.DataAnnotations;
+
+public class EncryptModel
  {
+
     public string Plaintext { get; set; }
 
+    [Required]
     public string Key { get; set; }
 
     public string Ciphertext { get; set; }
